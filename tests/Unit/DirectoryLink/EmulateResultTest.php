be71bea04739d5b1b6aa3aef7ec631e815d2e049
Anthony Edmonds <anthony.edmonds@networkrail.co.uk>
2026-08-27 09:17:10 +0100

    Fixed emulator output

--- tests/Unit/DirectoryLink/EmulateResultTest.php
+++ tests/Unit/DirectoryLink/EmulateResultTest.php
@@ -8,7 +8,7 @@ use NetworkRailBusinessSystems\DirectoryLink\Tests\TestCase;
 
 class EmulateResultTest extends TestCase
 {
-    public function testGroup(): void
+    public function testListGroup(): void
     {
         $users = DirectoryLink::emulateResult(
             '/group/list',
@@ -19,7 +19,7 @@ class EmulateResultTest extends TestCase
         $this->assertCount(1, $users);
     }
 
-    public function testUser(): void
+    public function testListUser(): void
     {
         $users = DirectoryLink::emulateResult(
             '/user/list',
@@ -30,7 +30,34 @@ class EmulateResultTest extends TestCase
         $this->assertCount(1, $users);
     }
 
-    public function testSpecificUser(): void
+    public function testGet(): void
+    {
+        $user = DirectoryLink::emulateResult(
+            '/user/get',
+            'gandalf.stormcrow@networkrail.co.uk',
+            'mail',
+        );
+
+        $this->assertEquals(
+            'gandalf.stormcrow@networkrail.co.uk',
+            $user['mail'],
+        );
+    }
+
+    public function testExists(): void
+    {
+        $user = DirectoryLink::emulateResult(
+            '/user/exists',
+            'gandalf.stormcrow@networkrail.co.uk',
+            'mail',
+        );
+
+        $this->assertTrue(
+            $user['exists'],
+        );
+    }
+
+    public function testUserLogin(): void
     {
         $users = DirectoryLink::emulateResult(
             '/user/get',
